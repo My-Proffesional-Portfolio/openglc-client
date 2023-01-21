a50b7defd00dfef9f7014571763fb9cb71d76da9
Rolando <rolando.code@gmail.com>
2023-01-20 21:07:27 -0600

    Components

--- openglclevel-app/src/app/app.module.ts
+++ openglclevel-app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { RegisterComponent } from './Pages/register/register.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { NewRegisterComponent } from './Pages/new-register/new-register.component';
+import { AccountComponent } from './Services/account/account.component';
+import { MealEventsComponent } from './Services/meal-events/meal-events.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    NewRegisterComponent,
+    AccountComponent,
+    MealEventsComponent
   ],
   imports: [
     BrowserModule,

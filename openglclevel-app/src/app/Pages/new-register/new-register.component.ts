da0517f112249c6e45b018a25684636cd6d069d4
Rolando <rolando.code@gmail.com>
2023-01-21 22:50:15 -0600

    Login design

--- openglclevel-app/src/app/Pages/new-register/new-register.component.ts
+++ openglclevel-app/src/app/Pages/new-register/new-register.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-new-register',
   templateUrl: './new-register.component.html',
-  styleUrls: ['./new-register.component.css']
+  styleUrls: ['../login/login.component.css']
 })
 export class NewRegisterComponent implements OnInit {
 
--- openglclevel-app/src/app/app-routing.module.ts
+++ openglclevel-app/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Pages/login/login.component';
+import { NewRegisterComponent } from './Pages/new-register/new-register.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+    
+  },
+  {
+    path: 'register',
+    component: NewRegisterComponent,
     
   },
 ];
